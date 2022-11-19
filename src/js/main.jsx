408a364d1e70306ac6abafb6aa1e1ac16948164e
Ruslan Jafarov <83516044+ruslanjafarov@users.noreply.github.com>
2022-11-19 17:01:02 +0300

    react > all

---
+++ src/js/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import style from "../styles/style.scss"
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const elem = <h2>Hello, World!</h2>
+root.render(elem)
